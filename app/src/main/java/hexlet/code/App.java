cb8bcb6d1f412fe2a7c5d8884c3cf1a38d36d380
Aleksandr Pronichev <alexander.pronichew@yandex.ru>
2025-05-21 23:54:01 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,6 +9,15 @@ import picocli.CommandLine.Option;
         description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
 
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    private String format = "stylish";
+
     @Override
     public void run() {
         System.out.println("Hello World!");

59a8f176aafc311a5c41653924ed780fbae887cb
Aleksandr Pronichev <alexander.pronichew@yandex.ru>
2025-06-23 22:29:24 +0300

    fix

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -7,4 +7,10 @@ public class Differ {
     public static String generate(Map<String, Object> data1, Map<String, Object> data2, String format) {
         return Formatter.format(format, data1, data2);
     }
+
+    public static String generate(String filepath1, String filepath2, String format) throws Exception {
+        Map<String, Object> data1 = (Map<String, Object>) Parser.parse(filepath1);
+        Map<String, Object> data2 = (Map<String, Object>) Parser.parse(filepath2);
+        return generate(data1, data2, format);
+    }
 }

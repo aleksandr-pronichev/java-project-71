0a1d76a2d189c9343a6aed71364c66c9a0ef9cf7
Aleksandr Pronichev <alexander.pronichew@yandex.ru>
2025-06-23 21:19:24 +0300

    fix

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -90,32 +90,32 @@ public class DifferTest {
         Map<String, Object> data1 = (Map<String, Object>) Parser.parse(filepath1);
         Map<String, Object> data2 = (Map<String, Object>) Parser.parse(filepath2);
         String expected = """
-        {
-            chars1: [a, b, c]
-          - chars2: [d, e, f]
-          + chars2: false
-          - checked: false
-          + checked: true
-          - default: null
-          + default: [value1, value2]
-          - id: 45
-          + id: null
-          - key1: value1
-          + key2: value2
-            numbers1: [1, 2, 3, 4]
-          - numbers2: [2, 3, 4, 5]
-          + numbers2: [22, 33, 44, 55]
-          - numbers3: [3, 4, 5]
-          + numbers4: [4, 5, 6]
-          + obj1: {nestedKey=value, isNested=true}
-          - setting1: Some value
-          + setting1: Another value
-          - setting2: 200
-          + setting2: 300
-          - setting3: true
-          + setting3: none
-        }
-        """;
+            {
+                chars1: [a, b, c]
+              - chars2: [d, e, f]
+              + chars2: false
+              - checked: false
+              + checked: true
+              - default: null
+              + default: [value1, value2]
+              - id: 45
+              + id: null
+              - key1: value1
+              + key2: value2
+                numbers1: [1, 2, 3, 4]
+              - numbers2: [2, 3, 4, 5]
+              + numbers2: [22, 33, 44, 55]
+              - numbers3: [3, 4, 5]
+              + numbers4: [4, 5, 6]
+              + obj1: {nestedKey=value, isNested=true}
+              - setting1: Some value
+              + setting1: Another value
+              - setting2: 200
+              + setting2: 300
+              - setting3: true
+              + setting3: none
+            }
+            """;
         String actual = Differ.generate(data1, data2, "stylish");
         assertEquals(expected.trim(), actual.trim());
     }
@@ -127,20 +127,20 @@ public class DifferTest {
         Map<String, Object> data1 = (Map<String, Object>) Parser.parse(filepath1);
         Map<String, Object> data2 = (Map<String, Object>) Parser.parse(filepath2);
         String expected = """
-        Property 'chars2' was updated. From [complex value] to false
-        Property 'checked' was updated. From false to true
-        Property 'default' was updated. From null to [complex value]
-        Property 'id' was updated. From 45 to null
-        Property 'key1' was removed
-        Property 'key2' was added with value: 'value2'
-        Property 'numbers2' was updated. From [complex value] to [complex value]
-        Property 'numbers3' was removed
-        Property 'numbers4' was added with value: [complex value]
-        Property 'obj1' was added with value: [complex value]
-        Property 'setting1' was updated. From 'Some value' to 'Another value'
-        Property 'setting2' was updated. From 200 to 300
-        Property 'setting3' was updated. From true to 'none'
-        """;
+            Property 'chars2' was updated. From [complex value] to false
+            Property 'checked' was updated. From false to true
+            Property 'default' was updated. From null to [complex value]
+            Property 'id' was updated. From 45 to null
+            Property 'key1' was removed
+            Property 'key2' was added with value: 'value2'
+            Property 'numbers2' was updated. From [complex value] to [complex value]
+            Property 'numbers3' was removed
+            Property 'numbers4' was added with value: [complex value]
+            Property 'obj1' was added with value: [complex value]
+            Property 'setting1' was updated. From 'Some value' to 'Another value'
+            Property 'setting2' was updated. From 200 to 300
+            Property 'setting3' was updated. From true to 'none'
+            """;
         String actual = Differ.generate(data1, data2, "plain");
         assertEquals(expected.trim(), actual.trim());
     }
